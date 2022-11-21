a851a527facfa4f4f10c83a95d0fb7d6671b1014
Michał Kaczmarczyk <michkacz96@gmail.com>
2022-11-21 15:57:05 +0100

    Added soft deletes

--- app/Models/TicketCategory.php
+++ app/Models/TicketCategory.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use illuminate\Database\Eloquent\SoftDeletes;
 
 class TicketCategory extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 }
