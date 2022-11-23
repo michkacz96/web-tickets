3843b79d12160dba5a7e8757072bc72dcbd323b3
Michał Kaczmarczyk <michkacz96@gmail.com>
2022-11-23 14:49:32 +0100

    factory for ticketcategories

--- database/seeders/TicketCategorySeeder.php
+++ database/seeders/TicketCategorySeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\TicketCategory;
 
 class TicketCategorySeeder extends Seeder
 {
@@ -13,6 +15,32 @@ class TicketCategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('ticket_categories')->insert([
+            'name' => 'Category 1',
+            'description' => 'Description of category 1'
+        ]);
+
+        DB::table('ticket_categories')->insert([
+            'name' => 'Category 2'
+        ]);
+
+        DB::table('ticket_categories')->insert([
+            'name' => 'Category 3',
+            'description' => 'Description of category 3'
+        ]);
+
+        DB::table('ticket_categories')->insert([
+            'name' => 'Category 4',
+            'description' => 'Description of category 4'
+        ]);
+
+        DB::table('ticket_categories')->insert([
+            'name' => 'Category 5'
+        ]);
+
+        DB::table('ticket_categories')->insert([
+            'name' => 'Category 6',
+            'description' => 'Description of category 6'
+        ]);
     }
 }
