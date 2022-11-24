dc14f74541459dda3049d84d94176124690b637a
Michał Kaczmarczyk <michkacz96@gmail.com>
2022-11-24 13:54:42 +0100

    created stickty footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,6 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-100">
+  <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -18,13 +18,32 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+  </head>
+  <body class="d-flex flex-column h-100">
+    
     <div id="app">
         @include('inc.navbar')
         <main class="py-4 container">
             @yield('content')
         </main>
     </div>
-</body>
+    @include('inc.footer')
+    
+  </body>
 </html>
