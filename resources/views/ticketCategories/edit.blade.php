18162d7282b6d31b142cd287184b4d2f1a406eab
Michał Kaczmarczyk <michkacz96@gmail.com>
2022-11-28 11:24:22 +0100

    Updated data for laravel Localization

--- resources/views/ticketCategories/edit.blade.php
+++ resources/views/ticketCategories/edit.blade.php
@@ -1,22 +1,21 @@
 @extends('layouts.app')
-
 @section('content')
-    <a href="{{url()->previous()}}" class="btn btn-primary btn-block">{{$texts['BTN_GO_BACK']}}</a>
+    <a href="{{url()->previous()}}" class="btn btn-primary btn-block">{{__('BTN_GO_BACK')}}</a>
 
     <hr class="my-3">
     
-    {!! Form::open(['url' => url($language.'/ticket-categories/'), 'action' => 'App\Http\Controllers\TicketCategoryController@update', $ticketCategory->id, 'method' => 'POST']) !!}
+    {!! Form::open(['url' => url('/ticket-categories/'), 'action' => 'App\Http\Controllers\TicketCategoryController@update', $ticketCategory->id, 'method' => 'POST']) !!}
         <div class="form-group">
-            {!! Form::label('name', $texts['TEXT_NAME']) !!}
-            {!! Form::text('name', $ticketCategory->name, $attributes=['class' => 'form-control', 'placeholder' => $texts['TEXT_NAME']]) !!}
+            {!! Form::label('name', __('TEXT_NAME')) !!}
+            {!! Form::text('name', $ticketCategory->name, $attributes=['class' => 'form-control', 'placeholder' => __('TEXT_NAME')]) !!}
         </div>
         <div class="form-group">
-            {!! Form::label('description', $texts['TEXT_DESCRIPTION']) !!}
-            {!! Form::text('description', $ticketCategory->description, $attributes=['class' => 'form-control', 'placeholder' => $texts['TEXT_DESCRIPTION']]) !!}
+            {!! Form::label('description', __('TEXT_DESCRIPTION')) !!}
+            {!! Form::text('description', $ticketCategory->description, $attributes=['class' => 'form-control', 'placeholder' => __('TEXT_DESCRIPTION')]) !!}
         </div>
         
         <div class="my-3">
-            {{Form::submit($texts['BTN_SAVE_CHANGES'], ['class' => 'btn btn-primary float-end'])}}
+            {{Form::submit(__('BTN_SAVE_CHANGES'), ['class' => 'btn btn-primary float-end'])}}
         </div>
     {!! Form::close() !!}
 @endsection
