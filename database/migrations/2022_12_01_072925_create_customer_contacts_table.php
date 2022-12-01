b59b5027a266b362100709b7001e690823b48ff2
Michał Kaczmarczyk <michkacz96@gmail.com>
2022-12-01 13:04:36 +0100

    Migration for customer contacts

--- database/migrations/2022_12_01_072925_create_customer_contacts_table.php
+++ database/migrations/2022_12_01_072925_create_customer_contacts_table.php
@@ -16,6 +16,11 @@ class CreateCustomerContactsTable extends Migration
         Schema::create('customer_contacts', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->softDeletes();
+            $table->foreignIdFor(App\Models\Customer::class)->references('id')->on('customers')->onDelete('cascade');;
+            $table->string('type', 1); //P - phone, E - email
+            $table->string('value');
+            $table->string('tags')->nullable();
         });
     }
 
@@ -26,6 +31,8 @@ class CreateCustomerContactsTable extends Migration
      */
     public function down()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
         Schema::dropIfExists('customer_contacts');
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
     }
 }
