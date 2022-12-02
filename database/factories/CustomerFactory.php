d034430c0ca61add1bb2a0e939bc133df975b90e
Michał Kaczmarczyk <michkacz96@gmail.com>
2022-12-02 17:17:42 +0100

    Added factories for user and customer

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -13,8 +13,19 @@ class CustomerFactory extends Factory
      */
     public function definition()
     {
+        $type = $this->faker->randomElement(['B', 'I']);
+        $name = $type == 'I' ? $this->faker->unique()->name() : $this->faker->unique->company();
+        $tin = $type == 'B' ? $this->faker->unique()->numberBetween(1000000000, 9999999999) : null;
         return [
-            //
+            'type' => $type,
+            'name' => $name,
+            'full_name' => $name,
+            'address' => $this->faker->streetName(),
+            'building_number' => $this->faker->buildingNumber(),
+            'postal_code' => $this->faker->postCode(),
+            'city' => $this->faker->city(),
+            'country' => $this->faker->country(),
+            'tin_ssn' => $tin
         ];
     }
 }
