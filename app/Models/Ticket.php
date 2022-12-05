b31e06a87d41fd590f7a229e8ee4a6fe296923ab
Michał Kaczmarczyk <michkacz96@gmail.com>
2022-12-05 15:06:04 +0100

    added table for tickets

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Ticket extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    private static $statuses = [
+        'N' => 'New',
+        'A' => 'Assigned',
+        'I' => 'In progress',
+        'C' => 'Closed'
+    ];
+
+    public ststic function getStatuses(){
+        return self::$statuses;
+    }
 }
--- database/migrations/2022_12_05_091448_create_tickets_table.php
+++ database/migrations/2022_12_05_091448_create_tickets_table.php
@@ -16,6 +16,14 @@ class CreateTicketsTable extends Migration
         Schema::create('tickets', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->softDeletes();
+            $table->string('title');
+            $table->string('description');
+            $table->string('status', 1)->default('N');
+            $table->foreignIdFor(App\Models\User::class)->references('id')->on('users');
+            $table->foreignIdFor(App\Models\Customer::class)->nullable()->references('id')->on('customers');
+            $table->foreignIdFor(App\Models\TicketCategory::class)->nullable()->references('id')->on('ticket_categories')->nullOnDelete();
+            $table->timestamp('closed_at')->nullable();
         });
     }
 
@@ -26,6 +34,8 @@ class CreateTicketsTable extends Migration
      */
     public function down()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
         Schema::dropIfExists('tickets');
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
     }
 }
