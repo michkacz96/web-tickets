6a0cb19c27ce38b93a4be41cb22bc41bf441a47e
Michał Kaczmarczyk <michkacz96@gmail.com>
2022-12-08 14:40:19 +0100

    Added time zones in footer for tests

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,10 +1,14 @@
 <footer class="footer mt-auto py-3 bg-light">
   <?php
-    $dt = new DateTime("now", new DateTimeZone('Europe/Warsaw'));
+    $warsaw = new DateTime("now", new DateTimeZone('Europe/Warsaw'));
+    $perth = new DateTime("now", new DateTimeZone('Australia/Perth'));
+    $toronto = new DateTime("now", new DateTimeZone('America/Toronto'));
   ?>
     <div class="container d-flex justify-content-between">
       <span class="text-muted">webtic v. 1.0.0</span>
       <span class="text-muted">SERVER TIME: {{date('Y-m-d H:i:s')}}</span>
-      <span class="text-muted">LOCAL TIME: {{$dt->format('Y-m-d, H:i:s')}}</span>
+      <span class="text-muted">WARSAW TIME: {{$warsaw->format('Y-m-d, H:i:s')}}</span>
+      <span class="text-muted">PERTH TIME: {{$perth->format('Y-m-d, H:i:s')}}</span>
+      <span class="text-muted">TORONTO TIME: {{$toronto->format('Y-m-d, H:i:s')}}</span>
     </div>
 </footer>
