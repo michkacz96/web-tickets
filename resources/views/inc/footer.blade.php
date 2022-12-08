ce808847298cf0162e26647312a06a3c1f073e25
Michał Kaczmarczyk <michkacz96@gmail.com>
2022-12-08 13:16:48 +0100

    Added local and server time in footer for tests

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,5 +1,10 @@
 <footer class="footer mt-auto py-3 bg-light">
-    <div class="container">
-      <span class="text-muted">Place sticky footer content here.</span>
+  <?php
+    $dt = new DateTime("now", new DateTimeZone('Europe/Warsaw'));
+  ?>
+    <div class="container d-flex justify-content-between">
+      <span class="text-muted">webtic v. 1.0.0</span>
+      <span class="text-muted">SERVER TIME: {{date('Y-m-d H:i:s')}}</span>
+      <span class="text-muted">LOCAL TIME: {{$dt->format('Y-m-d, H:i:s')}}</span>
     </div>
 </footer>
