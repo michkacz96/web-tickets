9085d815e5fbd444cc63d02c9c645a26b847dc12
Michał Kaczmarczyk <michkacz96@gmail.com>
2022-12-13 14:01:24 +0100

    Added fillable and relations with user and ticket

--- app/Models/TicketDetail.php
+++ app/Models/TicketDetail.php
@@ -9,4 +9,27 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class TicketDetail extends Model
 {
     use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'ticket_id',
+        'status',
+        'user_id',
+        'msg'
+    ];
+
+    /**
+     * Returns user data of given detail
+     * @return App\Models\User
+     */
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Returns user data of given detail
+     * @return App\Models\Ticket
+     */
+    public function ticket(){
+        return $this->belongsTo(Ticket::class);
+    }
 }
