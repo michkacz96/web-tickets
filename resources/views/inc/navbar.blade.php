81cf1fc2dda5fd82cb3dc159d8754b0e63cf5f7d
Michał Kaczmarczyk <michkacz96@gmail.com>
2022-12-14 16:32:01 +0100

    Added links for task list nad create a task in navbar

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -88,6 +88,20 @@
                         </a>
                     </div>
                 </li>
+                <!-- Tasks -->
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{__('Tasks')}}
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{route('tasks.index')}}">
+                            {{ __('Task list') }}
+                        </a>
+                        <a class="dropdown-item" href="{{route('tasks.create')}}">
+                            {{ __('Add task') }}
+                        </a>
+                    </div>
+                </li>
                 @endauth
             </ul>
 
